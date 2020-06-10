63f0e04e020d5500118ac9f012f814bef06c7969
Anna <admin@lensesrequired.com>
2020-06-10 17:14:51 -0500

    Revert "push art in public folder"
    This reverts commit 07443de761b5332edf86e89d9276d1dbdb92da64.

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import time
 app = Flask(__name__)
 
-# @app.route('/', defaults={'path': ''})
-@app.route('/time')
-def time_route():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
     return jsonify(time=time.time())
