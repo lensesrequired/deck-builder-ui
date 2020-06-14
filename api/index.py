2f2707aa487274f6d23ab1fee7b8ca09d1387047
Anna <admin@lensesrequired.com>
2020-06-14 18:03:58 -0500

    Remove API, show buttons and multiple cards

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-import time
-app = Flask(__name__)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return jsonify(time=time.time())
--- src/App.js
+++ src/App.js
@@ -1,57 +1,108 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import './App.css';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    axios.get('/api').then(async res => {
-      console.log(res);
-      const { data } = res;
-      setDate(data.time);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: [
+        {
+          metadata: {
+            'art': 'characters/03.png',
+            'name': 'Blind Dude',
+            'actions': {
+              'additions': [
+                {
+                  'type': 'Draw',
+                  'qty': 1
+                }
+              ],
+              'discardQty': 1,
+              'discardRequired': true,
+              'destroyQty': 0,
+              'destroyRequired': true,
+              'buyingPower': 2
+            },
+            'costBuy': 3,
+            'victoryPoints': 1
+          }
+        },
+        {
+          metadata: {
+            'art': 'characters/03.png',
+            'name': 'Blind Dude',
+            'actions': {
+              'additions': [
+                {
+                  'type': 'Draw',
+                  'qty': 1
+                }
+              ],
+              'discardQty': 1,
+              'discardRequired': true,
+              'destroyQty': 0,
+              'destroyRequired': true,
+              'buyingPower': 2
+            },
+            'costBuy': 3,
+            'victoryPoints': 1
+          }
+        }
+      ],
+      images: []
+    };
+  }
+
+  getCard = (metadata, callback) => {
+    return fetch('https://deck-builder-api.herokuapp.com/card', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(metadata)
+    }).then(async (response) => {
+      let reader = new FileReader();
+      reader.readAsDataURL(await response.blob());
+      reader.onloadend = function() {
+        let base64data = reader.result;
+        callback(base64data);
+      };
     });
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{ ' ' }
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{ ' ' }
-        was bootstrapped with{ ' ' }
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{ ' ' }
-        and contains three directories, <code>/public</code> for static assets,{ ' ' }
-        <code>/src</code> for components and content, and <code>/api</code>{ ' ' }
-        which contains a serverless <a href="https://golang.org/">Go</a>{ ' ' }
-        function. See{ ' ' }
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br/>
-      <h2>The date according to Go is:</h2>
-      <p>{ date ? date : 'Loading date...' }</p>
-    </main>
-  );
+  };
+
+  getCards = async () => {
+    const { cards } = this.state;
+    const images = [];
+    this.setState({ images });
+    await Promise.all(cards.map(async (card) => {
+      await this.getCard(card.metadata, (data) => {
+        images.push(data);
+        this.setState({ images });
+      });
+    }));
+  };
+
+  componentDidMount(prevProps, prevState, snapshot) {
+    this.getCards();
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Build a Deck</h1>
+        <button>Upload</button>
+        <button>Add a Card</button>
+        <button>Export Template</button>
+        <p>{ this.state.images.length ? 'Cards' : 'Loading image...' }</p>
+        <div style={ { display: 'flex' } }>
+          { this.state.images.map(
+            (image) => (<div style={ { padding: '10px' } }><img style={ { height: '500px' } } src={ image }/></div>)) }
+        </div>
+        <button>Export Cards as PDF</button>
+        <button>Export Cards as JSON</button>
+        <button>Play Game!</button>
+      </main>
+    );
+  }
 }
 
 export default App;
