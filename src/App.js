4c0f33099ef5fdcf79b6ecb0be02ccc00f81ed8f
Anna <admin@lensesrequired.com>
2020-06-15 21:17:36 -0500

    Call new Deck endpoints

--- src/App.js
+++ src/App.js
@@ -8,41 +8,34 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      isLoading: false,
       cards: []
     };
     this.fileInputRef = React.createRef();
   }
 
-  getCard = (metadata, callback) => {
-    return new Promise((resolve => {
-      fetch('https://deck-builder-api.herokuapp.com/card', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(metadata)
-      }).then(async (response) => {
-        let reader = new FileReader();
-        reader.readAsDataURL(await response.blob());
-        reader.onloadend = function() {
-          let base64data = reader.result;
-          resolve(callback(base64data));
-        };
+  getCards = async () => {
+    this.setState({ isLoading: true });
+    fetch('https://deck-builder-api.herokuapp.com/deck/5ee8173ff5e32e48a1e6b1e4')
+      .then(async (response) => {
+        const deck = await response.json();
+        const { cards } = deck;
+
+        this.setState({ cards, isLoading: false });
       });
-    }));
   };
 
-  getCards = async () => {
-    let { cards } = this.state;
-    cards = await Promise.all(cards.map(async (card, index) => {
-      return new Promise(async resolve => {
-        if (!card.image) {
-          await this.getCard(card.metadata, (data) => {
-            card.image = data;
-          });
-        }
-        return resolve(card);
-      });
-    }));
-    this.setState({ cards });
+  updateCards = async (cards) => {
+    fetch('https://deck-builder-api.herokuapp.com/cards/5ee8173ff5e32e48a1e6b1e4', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(cards.map((c) => {
+        delete c.image;
+        return c;
+      }))
+    }).then(() => {
+      this.getCards();
+    });
   };
 
   updateQty = (index, value) => {
@@ -56,23 +49,19 @@ class App extends React.Component {
   removeCard = (index) => {
     const { cards } = this.state;
     cards.splice(index, 1);
-    this.setState({ cards });
+    this.updateCards(cards);
   };
 
   fileChange = ({ target }) => {
-    const cards = JSON.parse(JSON.stringify(this.state.cards));
+    const { cards } = this.state;
     let reader = new FileReader();
     const setCards = (c) => {
-      this.setState({ cards: c });
+      this.updateCards(c);
     };
     reader.onload = function onReaderLoad(event) {
       let obj = JSON.parse(event.target.result);
       if (Array.isArray(obj)) {
-        obj.forEach((card) => {
-          // TODO: validate card
-          cards.push({ metadata: card });
-        });
-        setCards(cards);
+        setCards([...cards, ...obj]);
       } else {
         // TODO: Show error
       }
@@ -82,12 +71,6 @@ class App extends React.Component {
 
   componentDidMount(prevProps, prevState, snapshot) {
     this.getCards();
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (JSON.stringify(prevState.cards) !== JSON.stringify(this.state.cards)) {
-      this.getCards();
-    }
   }
 
   render() {
@@ -105,19 +88,16 @@ class App extends React.Component {
         <Button><a href={ `${ process.env.PUBLIC_URL }/deck_template.json` } download={ `deck_template.json` }>
           Download Template</a></Button>
         <div style={ {
-          display: 'flex',
-          width: '100vw',
-          height: '80vh',
-          margin: '5px',
-          backgroundColor: 'lightgray',
-          overflowY: 'scroll',
-          flexWrap: 'wrap'
+          display: 'flex', width: '100vw', height: '80vh',
+          overflowY: 'scroll', flexWrap: 'wrap', margin: '5px',
+          backgroundColor: 'lightgray'
         } }>
-          { this.state.cards.map(
+          { this.state.isLoading ? <p>{ 'LOADING' }</p> : this.state.cards.map(
             (card, index) => (
               <div style={ { padding: '10px', display: 'flex', flexDirection: 'column' } }>
                 { card.image ?
-                  <img alt={ 'card' } style={ { height: '400px', marginBottom: '10px' } } src={ card.image }/> :
+                  <img alt={ 'card' } style={ { height: '400px', marginBottom: '10px' } }
+                       src={ `data:image/png;base64,${ card.image }` }/> :
                   'Loading...' }
                 <Input label={ 'Qty' } style={ { marginBottom: '3px' } } type={ 'number' } min={ 1 }
                        value={ card.qty || '1' } onChange={ (_, { value }) => this.updateQty(index, value) }/>
